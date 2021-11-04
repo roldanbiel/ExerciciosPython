3f5667e68a699f20268d01315b68a6308f54a096
Gabriel Roldan <roldanbiel@gmail.com>
2021-11-04 16:15:07 -0300

    Create ex3.py

---
+++ Seção04/ex3.py
@@ -0,0 +1,13 @@
+"""
+Peça para o usuário digitar 3 valores inteiros e imprima a soma.
+"""
+
+soma = 0
+
+numero = int(input('Digite o primeiro valor: '))
+numero1 = int(input('Digite o segundo valor: '))
+numero2 = int(input('Digite o terceiro valor: '))
+
+soma = (numero + numero1 + numero2)
+
+print(soma)
