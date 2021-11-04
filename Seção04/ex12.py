9b2f3840a866fbf8021e02a5032d54958ae3a5a6
Gabriel Roldan <roldanbiel@gmail.com>
2021-11-04 16:18:42 -0300

    Create ex12.py

---
+++ Seção04/ex12.py
@@ -0,0 +1,11 @@
+"""
+Leia uma distância em milhas e converta para Km.
+Fórmula de conversão: K = 1.61 * M.
+Sendo K a distância em Km e M a distância em milhas.
+"""
+
+milhas = float(input('Digita a distância em milhas: '))
+
+km = 1.61 * milhas
+
+print(km)
