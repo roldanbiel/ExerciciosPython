9548c53757ca0442a383043840d565384c9fbe5e
Gabriel Roldan <roldanbiel@gmail.com>
2021-11-04 16:18:59 -0300

    Create ex38.py

---
+++ Seção04/ex38.py
@@ -0,0 +1,11 @@
+"""
+Leia o salário de um funcionário e aumente 25%.
+"""
+
+salario = float(input('Valor do salário: '))
+
+aumento = salario * 0.25
+
+salario_final = salario + aumento
+
+print('Salário final é de:', salario_final, 'reais.')
