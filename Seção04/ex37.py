e4b481e8fb1da407bcafe792a47c718bcbf63f5b
Gabriel Roldan <roldanbiel@gmail.com>
2021-11-04 16:18:57 -0300

    Create ex37.py

---
+++ Seção04/ex37.py
@@ -0,0 +1,11 @@
+"""
+Leia o valor de um produto, tire 12% de desconto e imprima.
+"""
+
+valor = float(input('Valor do produto: '))
+
+desconto1 = valor * 0.12
+
+desconto = valor - desconto1
+
+print(desconto)
