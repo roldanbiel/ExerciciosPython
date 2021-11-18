b3d0fb47e825dece808e02f2ffd8915ae7d6314e
Gabriel Roldan <roldanbiel@gmail.com>
2021-11-18 10:03:27 -0300

    Create ex pintura.py

---
+++ Curso em Video/ex pintura.py
@@ -0,0 +1,7 @@
+
+largura = float(input('Digite a largura da parede: '))
+altura = float(input('Digite a altura da parede: '))
+
+area = largura * altura
+tinta = area / 2
+print('Tendo uma área de {} metros, é necessário de {} litros de tinta.'.format(area, tinta))
