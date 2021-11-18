f67e3a8b00698e2506d1e076e80f23c9c2fbf8f9
Gabriel Roldan <roldanbiel@gmail.com>
2021-11-18 10:03:39 -0300

    Create ex dobro, triplo e raiz.py

---
+++ Curso em Video/ex dobro, triplo e raiz.py
@@ -0,0 +1,8 @@
+
+numero = int(input('Digite um número: '))
+
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** 0.5
+
+print('Dobro: {} \nTriplo: {} \nRaiz: {}'.format(dobro, triplo, raiz))
