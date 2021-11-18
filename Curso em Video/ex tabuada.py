e36452e0ae51479ebae67291b79c1972dfed68e0
Gabriel Roldan <roldanbiel@gmail.com>
2021-11-18 10:03:57 -0300

    Create ex tabuada.py

---
+++ Curso em Video/ex tabuada.py
@@ -0,0 +1,13 @@
+
+numero = int(input('Digite um número para ver sua tabuada: '))
+
+print('{} x  {} = {}'.format(numero, 1, numero * 1))
+print('{} x  {} = {}'.format(numero, 2, numero * 1))
+print('{} x  {} = {}'.format(numero, 3, numero * 1))
+print('{} x  {} = {}'.format(numero, 4, numero * 1))
+print('{} x  {} = {}'.format(numero, 5, numero * 1))
+print('{} x  {} = {}'.format(numero, 6, numero * 1))
+print('{} x  {} = {}'.format(numero, 7, numero * 1))
+print('{} x  {} = {}'.format(numero, 8, numero * 1))
+print('{} x  {} = {}'.format(numero, 9, numero * 1))
+print('{} x {} = {}'.format(numero, 10, numero * 1))
