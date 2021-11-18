e0ba5b903c0088f05a786c58493fac5784e6ef5c
Gabriel Roldan <roldanbiel@gmail.com>
2021-11-18 10:06:25 -0300

    Create ex1.py

---
+++ Seção05/ex1.py
@@ -0,0 +1,12 @@
+"""
+Faça um programa que receba dois números e mostre
+qual deles é o maior.
+"""
+
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+
+if numero1 > numero2:
+    print('O número', numero1, 'é maior que', numero2)
+else:
+    print('O número', numero2, 'é maior que', numero1)
